12fe2c9200aa44846e71e7807cbb48e59f1b6d0d
Kapuu <kapuucompilations@gmail.com>
2026-02-16 18:37:33 -0300

    Cargo release rows are now represented by a class with all it's attributes.

--- changeBg.js
+++ changeBg.js
@@ -15,30 +15,87 @@ function captureRows(){
 }
 
 let results = captureRows()
-// What we look for is this attribute results[1].childNodes[0].childNodes[0]
-console.log(results[1].childNodes[0].childNodes[0])
 
 /*for(i=0; i < results.length; i++){
     results[i].style.backgroundColor = "red";
 };*/
 
-/*class CargoReleaseRow{
+console.log(results[0])
+
+class CargoReleaseRow{
     constructor(
         DOMRefer
     ){
-        this.DOMRefer = DOMRefer
-        this.pieces = this.get_pieces()
+        this.DOMRefer = DOMRefer;
+        this.pieces = this.getPieces(this.DOMRefer);
+        this.warehouse = this.getWarehouse(this.DOMRefer);
+        this.dimensionsContent = this.getDimensionsContent(this.DOMRefer);
+        [this.large, this.width, this.height] = this.parseDimensions(this.dimensionsContent);
+        this.weightContent = this.getWeightContent(this.DOMRefer)
+        this.weightUnit = this.getWeightUnits(this.weightContent)
+        this.weight = this.parseWeight(this.weightContent)
         //etc...
     }
 
-    calculate_individual_weight(this) {
-        return this.weight / this.pieces
+    getPieces(DOMRefer){
+        return Number(DOMRefer.childNodes[0].childNodes[0].childNodes[1].textContent)
     }
 
-    check_nonrackeable(this){
-        // do stuff
+    getWarehouse(DOMRefer){
+        return String(DOMRefer.childNodes[0].childNodes[0].childNodes[2].textContent)
     }
-}*/
+
+    getDimensionsContent(DOMRefer){
+        return String(DOMRefer.childNodes[0].childNodes[0].childNodes[3].textContent)
+    }
+
+    getWeightContent(DOMRefer){
+        return DOMRefer.childNodes[0].childNodes[0].childNodes[6].textContent
+    }
+
+    getWeightUnits(weightContent) {
+        if (weightContent.includes("kg")) {
+            return "kg";
+        } 
+        else if (weightContent.includes("lb")) {
+            return "lb";
+        }
+        return null;
+    }
+
+    parseWeight(weightContent){
+        return Number(weightContent.split(this.weightUnit)[0])
+    }
+
+    parseDimensions(dimensionsContent){
+        const dimensionsContentCleaned = dimensionsContent.replace("in", "");
+        const dimensions = dimensionsContentCleaned.split("x").map(Number);
+
+        return [dimensions[0], dimensions[1], dimensions[2]]
+    }
+
+    checkNonRack(){
+        if (this.large >= 92 || this.width >= 92 || this.height >= 84){
+            this.DOMRefer.childNodes[0].childNodes[0].childNodes[0].textContent += "NON RACK \n"
+        }
+    }
+
+    checkExtraDim(){
+        if (this.large >= 144 || this.width >= 144 || this.height >= 144){
+            this.DOMRefer.childNodes[0].childNodes[0].childNodes[0].textContent += "EXTRADIM \n"
+        }
+    }
+}
+
+let crr;
+
+for(i=0; i < results.length; i++){
+    crr = new CargoReleaseRow(results[i])
+    crr.checkNonRack()
+    crr.checkExtraDim()
+    console.log(crr)
+};
+
 
 
 
